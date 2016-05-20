1a0500fff4f7453b800a97a3df394a42b6f78c80
Albert <albert.cheu@gmail.com>
2016-05-20 19:21:11 -0400

    Python post generation; test post

---
+++ _posts/2016-05-20-A-thank-you.md
@@ -0,0 +1,10 @@
+---
+layout: post
+title:  "A thank you"
+date:   2016-05-20 19:12:04
+categories: jekyll update
+---
+
+This site is made possible by Github Pages and Jekyll.
+
+I have also used [an example from Matthew James Taylor](http://matthewjamestaylor.com/blog/bottom-footer-demo.htm) and Python documentation. So kudos to them, too.
---
+++ _posts/newpost.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+import sys
+from datetime import datetime
+#date:   2016-05-20 3:58:34 -0400
+
+frontmatter = """---
+layout: post
+title:  "{}"
+date:   {}
+categories: jekyll update
+---
+"""
+
+title = " ".join(sys.argv[1:])
+now = datetime.now()
+date = "{:%Y-%m-%d %H:%M:%S}".format(now)
+output = frontmatter.format(title,date)
+
+date = "{:%Y-%m-%d-%H-%M}".format(now)
+title = "-".join(sys.argv[1:])
+f = open(date+"-"+title+".md",'w')
+f.write(output)
+f.close()

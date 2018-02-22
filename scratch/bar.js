4c86821fa2734e7f9bb29fb1dd0c906fe81526c7
Albert <albert.cheu@gmail.com>
2018-02-22 15:05:20 -0500

    Axes in bar chart; toggle between visuals works as intended

--- scratch/bar.js
+++ scratch/bar.js
@@ -5,6 +5,16 @@ function toNumber(d) {
 }
 
 function barchart(){
+    //hide scatter plot
+    d3.select(".scatterplot").style("opacity",0)
+
+    //if we made a barchart already, show it and quit
+    var bc = d3.select(".barchart");
+    if (! bc.empty()) {
+	bc.style("opacity",1);
+	return;
+    }
+    
     var width = 960,
 	height = 500;
 
@@ -12,7 +22,8 @@ function barchart(){
 	width = 960 - margin.left - margin.right,
 	height = 500 - margin.top - margin.bottom;
 
-    var fitWidth = d3.scaleBand().range([0,width])
+    var fitWidth = d3.scaleBand()
+	.range([0,width]).padding(0.1);
     
     var fitHeight = d3.scaleLinear()
 	.range([height,0]);
@@ -22,41 +33,46 @@ function barchart(){
     	.attr("width",width+margin.left+margin.right)    
     //all child objects will be offset by the margin using this anchor
 	.append("g").attr("transform",
-			  "translate(" + margin.left + "," + margin.top + ")");
-    
-    container.selectAll("*").remove();
+			  "translate(" + margin.left + "," + margin.top + ")")
+	.attr("class","barchart");
     
     d3.csv("https://albertcheu.github.io/scratch/sahd.csv",toNumber,
 	   function(error,data){
-	       //all axis objects will be offset using this second anchor
-	       container.append("g")
-		   .attr("transform", "translate(0," + height + ")")
-		   .call(d3.axisBottom(fitWidth));
-
 	       //plot only the first few rows to avoid skinny bars
-	       data = data.slice(1,40)
-	       fitWidth.domain(Array(40))
+	       data = data.slice(0,39)
+	       fitWidth.domain(data.map(function(d) { return d["row.names"]; }))
 	       fitHeight.domain([0, d3.max(data,function(d) { return d.age; })]);
-	       //console.log(data)
 
 	       var barWidth = width / data.length;
-	       //container.attr("height", barHeight * data.length);
 	       
 	       var bar = container.selectAll(".bar")
 		   .data(data)
 		   .enter().append("g")
-		   .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
+		   .attr("class","bar")
+		   .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; })
 	       
 	       bar.append("rect")
 		   .attr("y", function(d) { return fitHeight(d.age); })
 		   .attr("height", function(d) { return height - fitHeight(d.age); })
-		   .attr("width", barWidth - 1);
+		   .attr("width", barWidth - 1)
 
 	       bar.append("text")
 		   .attr("x", barWidth / 2)
 		   .attr("y", function(d) { return fitHeight(d.age) + 3; })
 		   .attr("dy", ".75em")
 		   .text(function(d) { return d.age; });
+
+	       bar.exit().remove();
+	       
+	       //all x axis ticks will be offset because they are in this grouping
+	       container.append("g")
+		   .attr("transform", "translate(0," + height + ")")
+		   .call(d3.axisBottom(fitWidth));
+	       	       //all x axis ticks will be offset because they are in this grouping
+	       container.append("g")
+		   .attr("transform", "translate(0,0)")
+		   .call(d3.axisLeft(fitHeight));
+
 	   }
 	  );
 }
--- scratch/scatter.js
+++ scratch/scatter.js
@@ -7,19 +7,29 @@ function toNumber(d) {
 }
 
 function scatterplot(){
-    var width = 960,
-	height = 500;
-    
+    //hide bar chart
+    d3.select(".barchart").style("opacity",0)//remove()
+
+    //if we made a scatterplot already, show it and quit
+    var sp = d3.select(".scatterplot");
+    if (! sp.empty()) {
+	sp.style("opacity",1);
+	return;
+    }
+
+    var margin = {top: 20, right: 30, bottom: 30, left: 40},
+	width = 960 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom;
+        
     var fitY = d3.scaleLinear().range([height-2,0]);
     var fitX = d3.scaleLinear().range([0,width-2])
 
     var container = d3.select("svg")
 	.attr("height",height)
-    	.attr("width",width);
-
-    container.selectAll("*").remove();
-  
-    d3.csv("sahd.csv",toNumber,
+    	.attr("width",width)
+	.append("g").attr("class","scatterplot")
+    
+    d3.csv("https://albertcheu.github.io/scratch/sahd.csv",toNumber,
 	   function(error,data){
 
 	       fitY.domain([d3.min(data,function(d) { return d.obesity; }),
@@ -30,10 +40,13 @@ function scatterplot(){
 	       var point = container.selectAll(".point")
 		   .data(data)
 		   .enter().append("g")
+	       	   .attr("class","point")
 		   .attr("transform", function(d, i) { return "translate("+fitX(d.adiposity)+","+fitY(d.obesity)+")"});
 	       
 	       point.append("circle")
-		   .attr("r",2);	       
+		   .attr("r",2);
+
+	       point.exit().remove();
 	   }
 	  );
 }

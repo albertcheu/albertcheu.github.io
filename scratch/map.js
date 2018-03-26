81b0a5cd1382c5a0da071a4b7f2043f81b4a98ef
Albert <albert.cheu@gmail.com>
2018-03-26 16:28:44 -0400

    wide select2; comments

--- scratch/map.js
+++ scratch/map.js
@@ -1,68 +1,64 @@
 
+//constants and objects for zooming
 var minZoom = 1;
 var maxZoom = 8;
-
 var zoom = d3.zoom()
     .scaleExtent([minZoom, maxZoom])
     .on("zoom", zoomed);
 var identity = d3.zoomIdentity;
 
-var svg, active, map, path;
+var svg, map, active, path;
+
+//dimensions of the map
 var width=960;
 var height=500;
 
-//$(document).ready(function(){
-
-//});
-
 function worldmap(){
-    var ispSelector = d3.select("#ispSelector")
-    for (var i = 0; i < 10; i++){
-    	ispSelector.append("option").attr("value",i).html("abcdefghijklmnop "+i);
-    }
-    
-    $("#ispSelector").select2({placeholder:"Select ISPs and carriers"});
-    //d3.select(".select2-selection .select2-selection__rendered .select2-search__field")
-//	.attr("width",0.9*d3.select(".leftpanel").node().getBoundingClientRect().width);
-    
-    svg = d3.select("svg");
+    //populate the map svg
+    svg = d3.select("#map");
     active = d3.select(null);
 
+    //svg has to fit inside the right div
     width = 0.9*d3.select(".rightpanel").node().getBoundingClientRect().width;
     var scalingFactor = width / 960;
     height *= scalingFactor;
-    console.log(scalingFactor);
+    //console.log(scalingFactor);
 
-    setsize(width,height);
-    
-    svg.append("rect").attr("width",width).attr("height",height).on("click",reset);
-    
+    setsize("#map",width,height);
+
+    //the ocean
+    var ocean = svg.append("rect").attr("id","ocean");
+    ocean.on("click",reset);
+    setsize("#ocean",width,height);
     
     //all child objects will be inside this group
     map = svg.append("g")
 	.attr("class","worldmap");
     
-    var projection = d3.geoMercator()
-	//.translate([width / 2, 3*height / 5]);
-    path = d3.geoPath()
-	.projection(projection);
+    var projection = d3.geoMercator();
+    path = d3.geoPath().projection(projection);
 
+    //account for the resizing
     identity = d3.zoomIdentity
 	.translate(0, height / 6)
 	.scale(scalingFactor);
     svg.call( zoom.transform, identity);
-    
+
+    //draw the countries
     d3.json('http://albertcheu.github.io/scratch/bostock_topo.json',
 	    function(error, mapData) {
 		
 		var geojsonData = topojson.feature(mapData,mapData.objects.countries).features;
 		fixMap(geojsonData);
-		
+
+		//country = "path" element
 		map.selectAll("path").data(geojsonData)
 		    .enter().append("path")
 		    .attr('d', path)
-		    .attr('class','country').on("click",clicked);
-		
+		    .attr('class','country')
+		    .on("click",clicked);
+
+		//the following is necessary for good-looking borders
 		map.append("path")
 		    .datum(topojson.mesh(mapData,mapData.objects.countries,
 					 function(a, b) { return a !== b; }))
@@ -70,9 +66,25 @@ function worldmap(){
 		    .attr("d", path);
 	    }
 	   );
+
+    //populate the isp selector
+    var ispSelector = d3.select("#ispSelector")
+    for (var i = 0; i < 10; i++){
+    	ispSelector.append("option").attr("value",i).html("ISP number "+i);
+    }
+    
+    //make sure it is wide enough
+    $("#ispSelector").select2({placeholder:"Select ISPs and carriers"});
+    d3.select(".select2").attr("style","width:100%;");
+    d3.select(".select2-selection__rendered").attr("style","width:100%;");
+    d3.select(".select2-search__field").attr("style","width:100%;");
+
 }
 
+//the callback for clicking on a country
 function clicked(d) {
+    console.log("You clicked on the country with ISO code "+d.id);
+
     //if I clicked the selected country, reset view
     if (active.node() === this) {
 	return reset();
@@ -82,17 +94,16 @@ function clicked(d) {
     active.classed("active", false);
     active = d3.select(this).classed("active", true);
 
+    //pan & zoom variables
     var bounds = path.bounds(d),
 	dx = bounds[1][0] - bounds[0][0],
 	dy = bounds[1][1] - bounds[0][1],
 	x = (bounds[0][0] + bounds[1][0]) / 2,
 	y = (bounds[0][1] + bounds[1][1]) / 2,
 	scale = Math.max(minZoom, Math.min(maxZoom, 0.9 / Math.max(dx / width, dy / height))),
-	//scale = 8,
 	translate = [width / 2 - scale * x, height / 2 - scale * y];
 
-    //console.log(translate);
-    
+    //actually pan & zoom
     svg.transition()
 	.duration(750)
 	.call( zoom.transform,
@@ -100,6 +111,7 @@ function clicked(d) {
 	       .scale(scale) ); // updated for d3 v4
 }
 
+//for when you click on a selected country or the ocean
 function reset() {
     active.classed("active", false);
     active = d3.select(null);    
@@ -108,6 +120,7 @@ function reset() {
       .call( zoom.transform, identity ); // updated for d3 v4
 }
 
+//something from Bostock that is necessary for zooming
 function zoomed() {
     map.style("stroke-width", 1.5 / d3.event.transform.k + "px");
     map.attr("transform", d3.event.transform); // updated for d3 v4
@@ -125,6 +138,7 @@ function fixMap(geojsonData){
     
     //spare ids start from 900
     var spareID = 900;
+
     
     //France (iso 250) is at index 72
     //France consists of Metro (European) France and Overseas France
@@ -157,7 +171,5 @@ function fixMap(geojsonData){
 	    spareID++;
 	}
     }
-
-    //console.log(spareID);
 
 }

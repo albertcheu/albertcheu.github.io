4667c5a988198e473c43ef4132a87b5441c00a3b
Albert <albert.cheu@gmail.com>
2018-04-02 01:33:11 -0400

    updated shared.js

--- scratch/shared.js
+++ scratch/shared.js
@@ -2,5 +2,8 @@ function setsize(selection,width,height){
     
     var container = d3.select(selection)
     container.attr("height",height);
-    container.attr("width",width);    
+    container.attr("width",width);
+
+    container.style("height",height);
+    container.style("width",width);
 }
